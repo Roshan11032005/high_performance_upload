2f6433da66ec7654fed10398a72db0b03899b1fe
junaid <shaikjunaid323@gmail.com>
2026-02-01 03:48:39 +0530

    lasdjk :

---
+++ flask_webserver/webserver/db/storage.py
@@ -0,0 +1,12 @@
+import json
+
+from sqlalchemy import text
+from datetime import datetime, timezone
+
+from webserver import db
+
+
+def insert_s3_metadata(email_id: str, file_path: str, size: int):
+    query = "INSERT INTO TABLE BUCKET_INFO (email_id, file_path, size) VALUES (:email_id, :file_path, :size)"
+    with db.engine.begin() as conn:
+        conn.execute(text(query), {"email_id": email_id, "file_path": file_path, "size": size})
--- flask_webserver/webserver/views/auth.py
+++ flask_webserver/webserver/views/auth.py
@@ -16,12 +16,13 @@ from flask_jwt_extended import (
     set_refresh_cookies,
     jwt_required,
     get_jwt_identity,
-    unset_jwt_cookies
+    unset_jwt_cookies,
 )
 import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from webserver.db import user as User
+from webserver.db import storage as Storage
 
 auth_bp = Blueprint("auth", __name__)
 
@@ -30,42 +31,45 @@ auth_bp = Blueprint("auth", __name__)
 def index():
     return redirect("https://google.com")
 
-@auth_bp.route('/login', methods=['GET', 'POST'])
+
+@auth_bp.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        email_id = request.form['username']
-        password = request.form['password']
+    if request.method == "POST":
+        email_id = request.form["username"]
+        password = request.form["password"]
         user = User.get_user(email_id)
 
         if not user or not check_password_hash(user["password_hash"], password):
-            return jsonify({'message': 'Invalid email or password'}), 401
+            return jsonify({"message": "Invalid email or password"}), 401
 
         access_token = create_access_token(identity=email_id)
         refresh_token = create_refresh_token(identity=email_id)
         response = jsonify({"access_token": access_token})
         set_refresh_cookies(response, refresh_token)
-        
+
         return response
     else:
-        return render_template('login.html')
+        return render_template("login.html")
 
-@auth_bp.route('/register', methods=['GET', 'POST'])
+
+@auth_bp.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == 'POST':
-        email_id = request.form['username']
-        password = request.form['password']
+    if request.method == "POST":
+        user_name = request.form["user_name"]
+        email_id = request.form["email_id"]
+        password = request.form["password"]
         public_id = str(uuid.uuid4())
 
         existing_user = User.get_user(email_id)
         if existing_user:
-            return jsonify({'message': 'User already exists. Please login.'}), 400
+            return jsonify({"message": "User already exists. Please login."}), 400
 
         hashed_password = generate_password_hash(password)
-        User.register_user(email_id, hashed_password, public_id)
+        User.register_user(user_name, email_id, hashed_password, public_id)
 
-        return redirect(url_for('auth.login'))
+        return redirect(url_for("auth.login"))
 
-    return render_template('register.html')
+    return render_template("register.html")
 
 
 @auth_bp.post("/refresh")
@@ -81,3 +85,18 @@ def logout():
     response = jsonify({"msg": "logged out"})
     unset_jwt_cookies(response)
     return response
+
+
+@auth_bp.post("/s3-metadata")
+@jwt_required
+def s3_metadata():
+    data = request.json
+    email_id = get_jwt_identity()
+    file_path = data["file_path"]
+    size = data["size"]
+    try:
+        Storage.insert_s3_metadata(email_id, file_path, size)
+    except Exception as err:
+        return jsonify({"error": err}), 500
+
+    return jsonify({"status": "okay"})
